fc36c0c92500adca58d6763eddb63b50908be506
Steven Jenkins <j.s.jenkins@jpl.nasa.gov>
2020-08-20 10:37:59 -0700

    owl-close-taxonomy renamed to owl-close-world

---
+++ owl-tools/owl-close-world/src/main/java/io/opencaesar/closeworld/Axiom.java
@@ -0,0 +1,100 @@
+package io.opencaesar.closeworld;
+
+import java.util.Arrays;
+import java.util.Set;
+
+public abstract class Axiom {
+
+    public enum AxiomType {DISJOINT_CLASSES, EQUIVALENT_CLASSES, DISJOINT_UNION}
+
+    public abstract static class ClassExpressionSetAxiom extends Axiom {
+
+        private final Set<ClassExpression> set;
+
+        protected ClassExpressionSetAxiom(Set<ClassExpression> set) {
+            super();
+            this.set = set;
+        }
+
+        protected Set<ClassExpression> getSet() {
+            return set;
+        }
+
+        public int hashCode() {
+            return set.hashCode();
+        }
+
+        public boolean equals(Object o) {
+            return (o instanceof ClassExpressionSetAxiom) && (((ClassExpressionSetAxiom) o).getSet().equals(getSet()));
+
+        }
+        public String toString(String type) {
+            return type + "(" + set.toString() + ")";
+        }
+
+        protected static class DisjointClassesAxiom extends ClassExpressionSetAxiom {
+
+            protected DisjointClassesAxiom(Set<ClassExpression> set) {
+                super(set);
+            }
+
+            @Override
+            public boolean equals(Object o) {
+                return (o instanceof DisjointClassesAxiom) && super.equals(o);
+            }
+
+            @Override
+            public String toString() {
+                return super.toString("DisjointClasses");
+            }
+        }
+
+        protected static class EquivalentClassesAxiom extends ClassExpressionSetAxiom {
+
+            protected EquivalentClassesAxiom(Set<ClassExpression> set) {
+                super(set);
+            }
+
+            @Override
+            public boolean equals(Object o) {
+                return (o instanceof EquivalentClassesAxiom) && super.equals(o);
+            }
+
+            @Override
+            public String toString() {
+                return super.toString("EquivalentClasses");
+            }
+        }
+
+        protected static class DisjointUnionAxiom extends ClassExpressionSetAxiom {
+
+            protected ClassExpression.Singleton c;
+
+            protected DisjointUnionAxiom(ClassExpression.Singleton c, Set<ClassExpression> set) {
+                super(set);
+                this.c = c;
+            }
+
+            protected ClassExpression.Singleton getC() {
+                return c;
+            }
+
+            @Override
+            public int hashCode() {
+                return Arrays.asList(c, getSet()).hashCode();
+            }
+
+            @Override
+            public boolean equals(Object o) {
+                return (o instanceof DisjointUnionAxiom) &&
+                        c.equals(((DisjointUnionAxiom) o).getC()) && super.equals(o);
+            }
+
+            @Override
+            public String toString() {
+                return "DisjointUnion(" + c.toString() + ", " + getSet().toString() + ")";
+            }
+        }
+    }
+
+}
---
+++ owl-tools/owl-close-world/src/test/java/io/opencaesar/closeworld/TestTaxonomyProperties.java
@@ -0,0 +1,111 @@
+package io.opencaesar.closeworld;
+
+import io.opencaesar.closeworld.ClassExpression.Singleton;
+import io.opencaesar.closeworld.Taxonomy.InvalidTreeException;
+import io.opencaesar.closeworld.Taxonomy.UnconnectedTaxonomyException;
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class TestTaxonomyProperties {
+	
+	private Singleton a;
+	private Singleton b;
+	private Singleton c;
+	
+	private Taxonomy connectedTree;
+	private Taxonomy connectedNotTree;
+	private Taxonomy notConnectedNotTree;
+	
+	@BeforeClass public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before public void setUp() throws Exception {
+		a = new Singleton("a");
+		b = new Singleton("b");
+		c = new Singleton("c");
+		
+		notConnectedNotTree = new Taxonomy();
+		notConnectedNotTree.addVertex(a);
+		notConnectedNotTree.addVertex(b);
+		notConnectedNotTree.addVertex(c);
+		
+		connectedTree = (Taxonomy) notConnectedNotTree.clone();
+		connectedTree.addEdge(a, b);
+		connectedTree.addEdge(a, c);
+		
+		connectedNotTree = (Taxonomy) connectedTree.clone();
+		connectedNotTree.addEdge(b, c);
+		
+	}
+
+	@After public void tearDown() throws Exception {
+	}
+
+	@Test public void testIsConnected() {
+		assertFalse(notConnectedNotTree.isConnected());
+		assertTrue(connectedTree.isConnected());
+		assertTrue(connectedNotTree.isConnected());
+	}
+
+	@Test public void testEnsureConnected() {
+		try {
+			notConnectedNotTree.ensureConnected();
+			fail("no UnconnectedTaxonomyException thrown");
+		}
+		catch (UnconnectedTaxonomyException e) {
+			assertTrue(true);
+		}
+		
+		try {
+			connectedTree.ensureConnected();
+			assertTrue(true);
+		}
+		catch (UnconnectedTaxonomyException e) {
+			fail("UnconnectedTaxonomyException thrown");
+		}
+
+		try {
+			connectedNotTree.ensureConnected();
+			assertTrue(true);
+		}
+		catch (UnconnectedTaxonomyException e) {
+			fail("UnconnectedTaxonomyException thrown");
+		}
+	}
+
+	@Test public void testIsTree() {
+		assertFalse(notConnectedNotTree.isTree());
+		assertTrue(connectedTree.isTree());
+		assertFalse(connectedNotTree.isTree());
+	}
+
+	@Test public void testEnsureTree() {
+		try {
+			notConnectedNotTree.ensureTree();
+			fail("no InvalidTreeException thrown");
+		}
+		catch (InvalidTreeException e) {
+			assertTrue(true);
+		}
+	
+		try {
+			connectedTree.ensureTree();
+			assertTrue(true);
+		}
+		catch (InvalidTreeException e) {
+			fail("InvalidTreeException thrown");
+		}
+
+		try {
+			connectedNotTree.ensureTree();
+			fail("no InvalidTreeException thrown");
+		}
+		catch (InvalidTreeException e) {
+			assertTrue(true);
+		}
+	}
+}
