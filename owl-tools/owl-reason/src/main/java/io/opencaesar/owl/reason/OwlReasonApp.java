0f8d4c9fde0e61bf9eec2d88db0bc1c6e619d198
Maged Elaasar <melaasar@gmail.com>
2020-09-16 13:07:33 -0700

    reverted JGrapht dependency to 1.1

--- owl-tools/owl-reason/src/main/java/io/opencaesar/owl/reason/OwlReasonApp.java
+++ owl-tools/owl-reason/src/main/java/io/opencaesar/owl/reason/OwlReasonApp.java
@@ -25,7 +25,6 @@ import java.net.URI;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.EnumSet;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
